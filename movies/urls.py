fb4af4fcdebb2249c20ca03cd3b049c325960620
Rodion1198 <rodka1198@gmail.com>
2021-06-22 00:54:01 +0300

    added detail page for actors and directors, added filtering by genre and year, added search movies by title

--- movies/urls.py
+++ movies/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.MoviesList.as_view()),
-    path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail')
+    path('filter/', views.FilterMovieView.as_view(), name='filter'),
+    path('search/', views.Search.as_view(), name='search'),
+    path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'),
+    path('actor/<str:slug>/', views.ActorView.as_view(), name='actor_detail'),
+
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,52 @@
+from django.db.models import Q
 from django.views import generic
 
-from .models import Movie
+from .models import Actor, Genre, Movie
 
 
-class MoviesList(generic.ListView):
+class GenreYear:
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values('year')
+
+
+class MoviesList(GenreYear, generic.ListView):
     """List of movies"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(generic.DetailView):
+class MovieDetailView(GenreYear, generic.DetailView):
 
     model = Movie
     slug_field = "url"
+
+
+class ActorView(GenreYear, generic.DetailView):
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = 'name'
+
+
+class FilterMovieView(GenreYear, generic.ListView):
+    def get_queryset(self):
+        qs = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year')) |
+            Q(genres__in=self.request.GET.getlist('genre'))
+        )
+        return qs
+
+
+class Search(generic.ListView):
+
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Movie.objects.filter(title__icontains=self.request.GET.get('q'))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
